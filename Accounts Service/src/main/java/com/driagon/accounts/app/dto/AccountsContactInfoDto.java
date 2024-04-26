c8ece47dfac5203a4fc86079fcb1890282885d79
Adrián González <g.adrian.gonzalez.90@gmail.com>
2024-04-25 23:50:10 -0600

    implemented Actuator refresh endpoint

--- Accounts Service/src/main/java/com/driagon/accounts/app/dto/AccountsContactInfoDto.java
+++ Accounts Service/src/main/java/com/driagon/accounts/app/dto/AccountsContactInfoDto.java
@@ -1,10 +1,21 @@
 package com.driagon.accounts.app.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
